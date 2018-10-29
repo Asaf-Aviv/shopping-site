7efcee032c698e6fbf0ffb98d5cc8ae78ee1ca00
unknown <asafaviv89@gmail.com>
2018-10-29 07:44:32 +0200

    ''

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,20 +1,3 @@
-import React from 'react';
-
-export const createListItem = (isSelected, value, funcHandler, classes) => (
-  <li
-    className={classes}
-    tabIndex="0"
-    role="option"
-    aria-selected={isSelected}
-    key={value}
-    value={value}
-    onClick={funcHandler}
-    onKeyDown={funcHandler}
-  >
-    {value}
-  </li>
-);
-
 export const calculatePrice = (price, salePerc) => +(price - price * (salePerc / 100)).toFixed(2);
 
 export const loadStateFromStorage = () => {
