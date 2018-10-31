bef422637141bfe9cc8e072302bdbb9a55f2c0cc
unknown <asafaviv89@gmail.com>
2018-10-31 08:20:08 +0200

    changed test names

--- models/product.test.js
+++ models/product.test.js
@@ -25,23 +25,23 @@ describe('Product model test', () => {
     await mongoose.connection.close();
   });
 
-  it('Should be a model', () => {
+  it('is a model', () => {
     expect(Product).toBeDefined();
   });
 
-  it('Should create a product', async () => {
+  it('create a product', async () => {
     const product = await Product.addProduct(fakeProduct1);
     expect(product.isNew).toStrictEqual(false);
   });
 
-  it('Should return products by page', async () => {
+  it('return products by page', async () => {
     await Promise.all([Product.addProduct(fakeProduct1), Product.addProduct(fakeProduct2)]);
 
     const numOfProducts = (await Product.fetchProductsByPage()).length;
     expect(numOfProducts).toStrictEqual(2);
   });
 
-  it('Should return products by regex', async () => {
+  it('find products by regex', async () => {
     await Promise.all([Product.addProduct(fakeProduct1), Product.addProduct(fakeProduct2)]);
 
     const productRegex = new RegExp('at', 'i');
@@ -49,7 +49,7 @@ describe('Product model test', () => {
     expect(numOfProducts).toStrictEqual(1);
   });
 
-  it('Should update product quantity', async () => {
+  it('updates the product quantity', async () => {
     const { _id: productId } = await Product.addProduct(fakeProduct1);
 
     await Product.updateQuantity(productId, 'Brown', 7);
@@ -58,18 +58,18 @@ describe('Product model test', () => {
     expect(updatedProduct.colors[0].quantity).toStrictEqual(9);
   });
 
-  it('Should throw when product name already exists', async () => {
+  it('throw when product name already exists', async () => {
     await Product.addProduct(fakeProduct1);
 
     await expect(Product.addProduct(fakeProduct1)).rejects.toThrow('E11000');
   });
 
-  it('Should throw when providing an invalid schema', async () => {
+  it('throw when providing an invalid schema', async () => {
     await expect(Product.addProduct(invalidProduct)).rejects.toThrow('products validation failed');
   });
 
   describe('Reviews', () => {
-    it('Should add a review', async () => {
+    it('add a review', async () => {
       const { _id: productId } = await Product.addProduct(fakeProduct1);
 
       await Product.addReview(productId, fakeReview);
@@ -78,7 +78,7 @@ describe('Product model test', () => {
       expect(updatedProduct.reviews.length).toStrictEqual(1);
     });
 
-    it('Should throw when missing body field', async () => {
+    it('throw when missing body field', async () => {
       const { _id: productId } = await Product.addProduct(fakeProduct1);
 
       await expect(
@@ -86,7 +86,7 @@ describe('Product model test', () => {
       ).rejects.toThrowError();
     });
 
-    it('Should throw when missing rating field', async () => {
+    it('throw when missing rating field', async () => {
       const { _id: productId } = await Product.addProduct(fakeProduct1);
 
       await expect(
@@ -94,7 +94,7 @@ describe('Product model test', () => {
       ).rejects.toThrowError();
     });
 
-    it('Should throw when missing name field', async () => {
+    it('throw when missing name field', async () => {
       const { _id: productId } = await Product.addProduct(fakeProduct1);
 
       await expect(
