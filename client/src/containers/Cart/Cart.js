2cd3f10d5af5509bfa033f2a1908279de4800d7b
unknown <asafaviv89@gmail.com>
2018-10-31 08:05:57 +0200

    refactor to functional component

--- client/src/containers/Cart/Cart.js
+++ client/src/containers/Cart/Cart.js
@@ -1,55 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { CartItemPropTypes } from '../../PropTypes';
 import OrderItemSummary from '../../components/OrderItemSummary';
 import List from '../../components/List';
 
-class Cart extends Component {
-  static propTypes = {
-    cart: PropTypes.shape({
-      products: PropTypes.arrayOf(CartItemPropTypes),
-      totalPrice: PropTypes.number,
-    }).isRequired,
-    orderHandler: PropTypes.func.isRequired,
-    removeFromCartHandler: PropTypes.func.isRequired,
+const Cart = ({ cart, orderHandler, removeFromCartHandler }) => {
+  const submitOrder = () => {
+    orderHandler(cart.products);
   };
 
-  submitOrder = () => {
-    const { orderHandler, cart: { products } } = this.props;
-    orderHandler(products);
-  };
-
-  render() {
-    const { cart, removeFromCartHandler } = this.props;
-
-    if (!cart.products.length) {
-      return (
-        <main>
-          <h1>Your cart is empty!</h1>
-          <Link to="/store">Shop Now!</Link>
-        </main>
-      );
-    }
-
+  if (!cart.products.length) {
     return (
       <main>
-        <h1 className="cart__title">My Cart</h1>
-        <List>
-          {cart.products.map((item, i) => (
-            <OrderItemSummary
-              key={`${item._id}${item.color}${item.size}`}
-              productIndex={i}
-              item={item}
-              removeProduct={() => removeFromCartHandler(i)}
-            />
-          ))}
-        </List>
-        <h1>{cart.totalPrice}</h1>
-        <button type="button" onClick={this.submitOrder}>Submit Order</button>
+        <h1>Your cart is empty!</h1>
+        <Link to="/store">Shop Now!</Link>
       </main>
     );
   }
-}
+
+  return (
+    <main>
+      <h1 className="cart__title">My Cart</h1>
+      <List>
+        {cart.products.map((item, i) => (
+          <OrderItemSummary
+            key={`${item._id}${item.color}${item.size}`}
+            item={item}
+            removeProduct={() => removeFromCartHandler(i)}
+          />
+        ))}
+      </List>
+      <h1>{cart.totalPrice}</h1>
+      <button type="button" onClick={submitOrder}>Submit Order</button>
+    </main>
+  );
+};
+
+Cart.propTypes = {
+  cart: PropTypes.shape({
+    products: PropTypes.arrayOf(CartItemPropTypes),
+    totalPrice: PropTypes.number,
+  }).isRequired,
+  orderHandler: PropTypes.func.isRequired,
+  removeFromCartHandler: PropTypes.func.isRequired,
+};
 
 export default Cart;
