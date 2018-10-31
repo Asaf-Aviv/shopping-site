0c8635e33813d0916ad9b28141d1a3a201e10e6f
unknown <asafaviv89@gmail.com>
2018-10-31 06:29:28 +0200

    removed files

--- client/src/components/CartItem/index.js
+++
@@ -1,5 +0,0 @@
-import { connect } from 'react-redux';
-import CartItem from './CartItem';
-import { removeFromCartHandler } from '../../actions/cartActions';
-
-export default connect(null, { removeFromCartHandler })(CartItem);
