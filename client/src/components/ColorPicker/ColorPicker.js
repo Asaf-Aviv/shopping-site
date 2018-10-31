8387e3ef66e13f8edbc9ab052a542a745990c5cd
unknown <asafaviv89@gmail.com>
2018-10-31 06:21:13 +0200

    Refactor to functional component

--- client/src/components/ColorPicker/ColorPicker.js
+++ client/src/components/ColorPicker/ColorPicker.js
@@ -1,45 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import DropDown from '../DropDown';
 import { ColorPropTypes } from '../../PropTypes';
 
 
-class ColorPicker extends Component {
-  static propTypes = {
-    colors: PropTypes.arrayOf(ColorPropTypes).isRequired,
-    chosenColor: ColorPropTypes.isRequired,
-    chooseColorHandler: PropTypes.func.isRequired,
-  }
-
-  chooseColor = (e) => {
-    const { colors, chooseColorHandler } = this.props;
+const ColorPicker = ({ colors, chosenColor, chooseColorHandler }) => {
+  const chooseColor = (e) => {
     const colorName = e.target.getAttribute('value');
-    const chosenColor = colors.find(colorObj => colorObj.color === colorName);
-    chooseColorHandler(chosenColor);
+    const newChosenColor = colors.find(colorObj => colorObj.color === colorName);
+    chooseColorHandler(newChosenColor);
   };
 
-  render() {
-    const { colors, chosenColor } = this.props;
+  if (!colors.length) return null;
 
-    if (!colors.length) return null;
+  return (
+    <div className="color-picker">
+      <DropDown
+        items={colors.map(({ color }) => color)}
+        chosen={chosenColor.color || ''}
+        chooseHandler={chooseColor}
+        text="Color"
+      />
+      {chosenColor.quantity !== undefined
+        && (chosenColor.quantity > 0
+          ? <p>{`Only ${chosenColor.quantity} left!`}</p>
+          : <p>Out of stock!</p>
+        )
+      }
+    </div>
+  );
+};
 
-    return (
-      <div className="color-picker">
-        <DropDown
-          items={colors.map(({ color }) => color)}
-          chosen={chosenColor.color || ''}
-          chooseHandler={this.chooseColor}
-          text="Color"
-        />
-        {chosenColor.quantity !== undefined
-          && (chosenColor.quantity > 0
-            ? <p>{`Only ${chosenColor.quantity} left!`}</p>
-            : <p>Out of stock!</p>
-          )
-        }
-      </div>
-    );
-  }
-}
+ColorPicker.propTypes = {
+  colors: PropTypes.arrayOf(ColorPropTypes).isRequired,
+  chosenColor: ColorPropTypes.isRequired,
+  chooseColorHandler: PropTypes.func.isRequired,
+};
 
 export default ColorPicker;
