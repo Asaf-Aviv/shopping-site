4c359043ca9450e6732656cc4cac77e7a4202db4
unknown <asafaviv89@gmail.com>
2018-11-07 16:35:34 +0200

    changed to a container

---
+++ client/src/containers/SearchBarResults/SearchBarResults.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBarItem from '../../components/SearchBarItem';
+import LoadingIndicator from '../../components/LoadingIndicator';
+import { ProductPropTypes } from '../../PropTypes';
+
+const displayInfo = error => (
+  <div className="search-bar__info">
+    {error
+      ? <h2>Something went wrong</h2>
+      : <h2>No products found</h2>}
+  </div>
+);
+
+const SearchBarResults = ({
+  isFocused, productName, products, isFetching, error,
+}) => (
+  <div className={`search-bar__results ${isFocused && productName ? 'search-bar__results--open' : ''}`}>
+    <ul className="search-bar__list">
+      { isFetching && <LoadingIndicator size={80} />}
+      {products.length > 0
+        && products.map(product => <SearchBarItem product={product} key={product._id} />)
+      }
+      {products.length === 0 && !isFetching && productName && displayInfo(error)}
+    </ul>
+  </div>
+);
+
+SearchBarResults.propTypes = {
+  isFocused: PropTypes.bool.isRequired,
+  productName: PropTypes.string,
+  products: PropTypes.arrayOf(ProductPropTypes).isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+};
+
+SearchBarResults.defaultProps = {
+  productName: null,
+};
+
+export default SearchBarResults;
