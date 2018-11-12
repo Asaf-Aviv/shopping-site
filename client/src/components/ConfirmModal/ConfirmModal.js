2769b07e09964693d302108bc4c194dc1d6ea628
unknown <asafaviv89@gmail.com>
2018-11-12 19:17:53 +0200

    modal

---
+++ client/src/components/ConfirmModal/ConfirmModal.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+
+import './ConfirmModal.sass';
+
+class ConfirmModal extends Component {
+  static propTypes = {
+    successHandler: PropTypes.func.isRequired,
+    declineHandler: PropTypes.func.isRequired,
+    text: PropTypes.string.isRequired,
+    danger: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    danger: false,
+  }
+
+  onSuccess = () => {
+    const { successHandler } = this.props;
+    successHandler();
+  }
+
+  onDecline = () => {
+    const { declineHandler } = this.props;
+    declineHandler();
+  }
+
+  render() {
+    const { danger, text } = this.props;
+
+    return createPortal(
+      <div className={`modal ${danger ? 'modal--danger' : ''}`}>
+        <div className="modal__info">
+          <h4 className="modal__title">{text}</h4>
+          <div className="modal__btn-group">
+            <button
+              type="button"
+              className="btn btn--default"
+              onClick={this.onDecline}
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              className={`btn ${danger ? 'btn--danger' : 'btn--primary'}`}
+              onClick={this.onSuccess}
+            >
+              Ok
+            </button>
+          </div>
+        </div>
+      </div>,
+      document.getElementById('modal-root'),
+    );
+  }
+}
+
+export default ConfirmModal;
