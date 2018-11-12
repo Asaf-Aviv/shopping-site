f46508b52fa35678232e734dc6c07337ae353239
unknown <asafaviv89@gmail.com>
2018-11-12 19:25:27 +0200

    converted to FC

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { ReviewPropTypes } from '../../PropTypes';
 import ReviewForm from '../ReviewForm';
@@ -6,64 +6,39 @@ import Review from '../Review';
 
 import './Reviews.sass';
 
-class Reviews extends PureComponent {
-  static propTypes = {
-    reviews: PropTypes.arrayOf(ReviewPropTypes).isRequired,
-    productId: PropTypes.string.isRequired,
-  }
+const Reviews = ({ reviews, productId, toggleReviews }) => (
+  <div
+    className="reviews__container reviews__container--open"
+    onClick={toggleReviews}
+    onKeyPress={toggleReviews}
+    role="link"
+    tabIndex="0"
+  >
+    <div className="reviews__wrapper" role="presentation" onClick={e => e.stopPropagation()}>
+      <ReviewForm productId={productId} />
+      <h1 className="reviews__header">Reviews</h1>
+      {reviews.length === 0 && <h3>No reviews yet</h3>}
+      {reviews.length > 0
+        && (
+          <ul className="reviews__list">
+            {reviews.map(review => (
+              <Review
+                key={review.name + review.timestamp}
+                review={review}
+                productId={productId}
+              />
+            ))}
+          </ul>
+        )
+      }
+    </div>
+  </div>
+);
 
-  state = {
-    isOpen: false,
-  }
-
-  toggleReviews = (e) => {
-    if (e.key === 'Enter' || !e.key) {
-      this.setState(prevState => ({
-        isOpen: !prevState.isOpen,
-      }));
-    }
-  }
-
-  stopPropagation = e => e.stopPropagation();
-
-  render() {
-    const { reviews, productId } = this.props;
-    const { isOpen } = this.state;
-    return (
-      <>
-        <button
-          type="button"
-          className="btn btn--secondary"
-          onClick={this.toggleReviews}
-          onKeyPress={this.toggleReviews}
-        >
-          Reviews
-        </button>
-        <div
-          className={`reviews__container ${isOpen && 'reviews__container--open'}`}
-          onClick={this.toggleReviews}
-          onKeyPress={this.toggleReviews}
-          role="link"
-          tabIndex="0"
-        >
-          <div className="reviews__wrapper" role="presentation" onClick={this.stopPropagation}>
-            <ReviewForm productId={productId} />
-            <h1 className="reviews__header">Reviews</h1>
-            {reviews.length === 0 && <h3>No reviews yet</h3>}
-            {reviews.length > 0
-              && (
-                <ul className="reviews__list">
-                  {reviews.map(review => (
-                    <Review key={review.name + review.timestamp} review={review} />
-                  ))}
-                </ul>
-              )
-            }
-          </div>
-        </div>
-      </>
-    );
-  }
-}
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(ReviewPropTypes).isRequired,
+  productId: PropTypes.string.isRequired,
+  toggleReviews: PropTypes.func.isRequired,
+};
 
 export default Reviews;
