22fa635c867657de5bf6ab4a6e7875248452d495
unknown <asafaviv89@gmail.com>
2018-11-20 05:06:38 +0200

    calculate reting when deleting a review

--- models/Product.js
+++ models/Product.js
@@ -52,22 +52,36 @@ productSchema.statics.updateQuantity = function (productId, color, quantity) {
 
 productSchema.methods.calculateRating = function () {
   const total = this.reviews.reduce((sum, reviewObj) => sum + reviewObj.rating, 0);
-  this.rating = (total / this.reviews.length).toFixed(1);
+  if (total) {
+    this.rating = (total / this.reviews.length).toFixed(1);
+  }
+  return this;
 };
 
 productSchema.statics.addReview = async function (_id, review) {
   const doc = await this.findById(_id);
   doc.reviews.unshift(review);
-  doc.calculateRating();
 
-  return doc.save();
+  return doc
+    .calculateRating()
+    .save();
 };
 
-productSchema.statics.deleteReview = function (productId, reviewId) {
-  return this.updateOne(
+productSchema.statics.deleteReview = async function (productId, reviewId) {
+  const result = await this.updateOne(
     { _id: productId },
     { $pull: { reviews: { _id: reviewId } } },
   );
+
+  const doc = await this.findById(productId);
+  const reviewIndex = doc.reviews.findIndex(({ _id }) => _id.toString() === reviewId);
+
+  doc
+    .calculateRating()
+    .reviews.splice(reviewIndex, 1);
+
+  doc.save();
+  return result;
 };
 
 const Product = mongoose.model('products', productSchema);
